3b29f473ee98a96ef3820d79b04a005f32181708
leo <leo.coyere94@gmail.com>
2025-03-21 14:29:49 +0100

    Playing with shadcn/ui & 21st dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
 import './App.css'
-import { Hero } from "./components/Hero"
-import CircularGallery from './reactbits/CircularGallery'
-import FuzzyText from './reactbits/FuzzyText'
-import ScrollReveal from './reactbits/ScrollReveal'
-
+import { Hero } from "./components/ui/animated-hero"
 function App() {
 
   return (
-    <>
-      {/* <Hero /> */}
-      {/* <div className="h-150 relative">
-        <CircularGallery bend={0} />
-      </div> */}
-      <div className="m-20">
-        <FuzzyText 
-          baseIntensity={0.1} 
-          color='#fff'
-        >
-          Welcome to the lab
-        </FuzzyText>
-      </div>
-      <p className="text-l mx-auto mt-10 text-white font-semibold columns-2 gap-8 max-w-screen-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum earum aliquam error explicabo, obcaecati deleniti quisquam maiores rem cumque optio architecto provident laboriosam delectus amet eum nobis ea blanditiis labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quibusdam rerum placeat voluptas optio minima consequuntur! Cumque cupiditate fuga molestias quis odit! Consequuntur dolor fugiat ut similique eius placeat autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quibusdam rerum placeat voluptas optio minima consequuntur! Cumque cupiditate fuga molestias quis odit! Consequuntur dolor fugiat ut similique eius placeat autem.</p>
-      <div style={{ height: '600px', position: 'relative' }}>
-        <CircularGallery bend={0} textColor="#ffffff" borderRadius={0.05} />
-      </div>
-      <div className="w-1/2 mx-auto my-50">
-        <ScrollReveal
-          baseOpacity={0}
-          enableBlur={true}
-          baseRotation={5}
-          blurStrength={10}
-          textClassName='text-white'
-        >
-          It's crazy all the things you can do here, sky is the limit! Like this text, it reveal itself when you scroll. Then you want to keep scrolling to see more. Scroll, scroll, SCROLL!
-        </ScrollReveal>
-      </div>
-    </>
+    <div className="block">
+      <Hero />
+    </div>
   )   
 }
 
---
+++ src/components/ui/animated-hero.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useMemo, useState } from "react";
+import { motion } from "framer-motion";
+import { MoveRight, Mail } from "lucide-react";
+
+function Hero() {
+  const [titleNumber, setTitleNumber] = useState(0);
+  const titles = useMemo(
+    () => ["amazing", "new", "wonderful", "beautiful", "smart"],
+    []
+  );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (titleNumber === titles.length - 1) {
+        setTitleNumber(0);
+      } else {
+        setTitleNumber(titleNumber + 1);
+      }
+    }, 2000);
+    return () => clearTimeout(timeoutId);
+  }, [titleNumber, titles]);
+
+  return (
+    <div className="w-full">
+      <div className="container mx-auto">
+        <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
+            <button className="flex items-center gap-2 px-4 py-2 rounded-md">
+              Check my portfolio <MoveRight className="w-4 h-4" />
+            </button>
+          <div className="flex gap-4 flex-col">
+            <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
+              <span className="text-spektr-cyan-50">This is something</span>
+              <span className="relative flex w-full justify-center overflow-hidden text-center md:pb-4 md:pt-1">
+                &nbsp;
+                {titles.map((title, index) => (
+                  <motion.span
+                    key={index}
+                    className="absolute font-semibold"
+                    initial={{ opacity: 0, y: "-100" }}
+                    transition={{ type: "spring", stiffness: 50 }}
+                    animate={
+                      titleNumber === index
+                        ? {
+                            y: 0,
+                            opacity: 1,
+                          }
+                        : {
+                            y: titleNumber > index ? -150 : 150,
+                            opacity: 0,
+                          }
+                    }
+                  >
+                    {title}
+                  </motion.span>
+                ))}
+              </span>
+            </h1>
+
+            <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
+              Managing a small business today is already tough. Avoid further
+              complications by ditching outdated, tedious trade methods. Our
+              goal is to streamline SMB trade, making it easier and faster than
+              ever.
+            </p>
+          </div>
+          <div className="flex flex-row gap-3">
+            {/* <Button size="lg" className="gap-4" variant="outline">
+              Browse my Github          
+              <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+              </svg>
+            </Button>
+            <Button size="lg" className="gap-4 bg-red-500 text-white">
+              Sign up here <Mail className="w-4 h-4" />
+            </Button> */}            
+            <button className="flex items-center gap-2 px-4 py-2 rounded-md">
+              Browse my Github
+              <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+              </svg>
+            </button>
+            <button className="flex items-center gap-2 px-4 py-2 rounded-md">
+              Mail me <Mail className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Hero };
